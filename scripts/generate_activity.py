7cf0cff7577ed31443f16f9ef3b4cbe638e34537
𝓐𝓻ف𝓪 𝓜𝓾ن𝓪𝓶 ❀ <arfamunam01@gmail.com>
2026-09-23 06:11:54 -0700

    Update generate_activity.py

--- scripts/generate_activity.py
+++ scripts/generate_activity.py
@@ -516,7 +516,7 @@ for col in range(columns):
 
 svg += text(
     "Mon",
-    85,
+    90,
     175,
     15,
     "#E9C6D6",
@@ -525,7 +525,7 @@ svg += text(
 
 svg += text(
     "Wed",
-    85,
+    90,
     210,
     15,
     "#E9C6D6",
@@ -534,7 +534,7 @@ svg += text(
 
 svg += text(
     "Fri",
-    85,
+    90,
     245,
     15,
     "#E9C6D6",
